97edde64e62a2d4d54e70bb32a3a8d26c3e83f1e
kaoshanji <1127102203@qq.com>
2019-05-28 11:04:27 +0800

    config-repo

---
+++ spring-cloud/config-git-rabbit/cloud-config-service/src/main/java/top/kaoshanji/ConfigServiceApp.java
@@ -0,0 +1,18 @@
+package top.kaoshanji;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApp.class, args);
+    }
+
+}
