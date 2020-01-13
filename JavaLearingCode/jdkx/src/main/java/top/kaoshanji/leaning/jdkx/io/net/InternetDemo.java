4bb4bf8a045cecac69a9f4de642f68dc23c358d8
kaoshanji-MacBookPro <1127102203@qq.com>
2020-01-13 23:35:11 +0800

    每天看一点

--- JavaLearingCode/jdkx/src/main/java/top/kaoshanji/leaning/jdkx/io/net/InternetDemo.java
+++ JavaLearingCode/jdkx/src/main/java/top/kaoshanji/leaning/jdkx/io/net/InternetDemo.java
@@ -39,14 +39,55 @@ public class InternetDemo {
     }
 
     /**
-     * 查找本地机器的地址
+     * 查找本地机器的IP地址
      * 一个真正的主机名和IP地址
      * @throws UnknownHostException
      */
-    public void getLocalHost() throws UnknownHostException {
+    public void getHostAddress() throws UnknownHostException {
         InetAddress address = InetAddress.getLocalHost();
-        logger.info("address:"+address);
+        logger.info("address:"+address.getHostAddress());
     }
+
+
+    /**
+     * 确定IP地址是 IPv4还是IPv6
+     * @param inetAddress
+     * @return
+     */
+    public int getVersion(InetAddress inetAddress) {
+        int length = -1;
+        byte [] address = inetAddress.getAddress();
+        if (address.length == 4) {
+            return 4;
+        }
+
+        if (address.length == 6) {
+            return 6;
+        }
+        return length;
+    }
+
+    /**
+     * 地址类型
+     * @throws UnknownHostException
+     */
+    public void ipCharacteristics() throws UnknownHostException {
+        InetAddress address = InetAddress.getByName("");
+
+        logger.info("是否是通配地址，可以匹配本地系统中的任何地址:"+address.isAnyLocalAddress());
+        logger.info("是否是回送地址，直接在IP层连接同一台计算机:"+address.isLoopbackAddress());
+        logger.info("是否是IPv6本地连接地址:"+address.isLinkLocalAddress());
+        logger.info("是否是IPv6本地网络地址:"+address.isSiteLocalAddress());
+
+        logger.info("是否是一个组播地址:"+address.isMulticastAddress());
+        logger.info("是否是一个组织范围组播地址:"+address.isMCOrgLocal());
+        logger.info("是否是一个网站范围组播地址:"+address.isMCSiteLocal());
+        logger.info("是否是一个子网范围组播地址:"+address.isMCLinkLocal());
+        logger.info("是否是一个本地接口组播地址:"+address.isMCNodeLocal());
+
+    }
+
+
 
 
 }
